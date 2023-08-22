b93474feea3b1bb77952ab29c30bd5746cb5af38
Nagasai <nagasai.nistala@gmail.com>
2023-08-22 23:55:44 +0530

    Updated code changes to include load screen animation and primary screen

--- vrapp/app.js
+++ vrapp/app.js
@@ -1,25 +1,20 @@
-const loadingScreen = document.getElementById('loading-screen');
-const vrScene = document.getElementById('vr-scene');
-const searchInput = document.getElementById('search-input');
-const searchButton = document.getElementById('search-button');
+AFRAME.registerComponent('log', {
+    schema: {type: 'string'},
 
-searchButton.addEventListener('click', async () => {
-    const searchTerm = searchInput.value;
-    
-    if (searchTerm) {
-        // Simulate AI-generated 360-degree image using Lorem Picsum
-        const response = await fetch(`https://picsum.photos/800/800?random=${searchTerm}`);
-        const imageUrl = response.url;
-        
-        // Hide loading screen and show VR scene
-        loadingScreen.style.display = 'none';
-        vrScene.style.display = 'block';
-        
-        // Create a 360-degree image element based on the generated URL
-        const image360 = document.createElement('img');
-        image360.setAttribute('src', imageUrl);
-        image360.setAttribute('crossorigin', 'anonymous');
-        image360.setAttribute('360-image', '');
-        vrScene.appendChild(image360);
+    init: function () {
+        var stringToLog = this.data;
+        console.log(stringToLog);
     }
 });
+setTimeout(() => {
+    $("#intro").hide();
+    $("#initial").show();
+}, 3500);
+function turnOnBgm(){
+    $("#bgmaudio").attr('src','assets/sounds/bgm.ogg');
+    $("#bgmsound").attr('src','assets/sounds/bgm.ogg');
+}
+function turnOffBgm(){
+    $("#bgmaudio").attr('src','');
+    $("#bgmsound").attr('src','');
+}
