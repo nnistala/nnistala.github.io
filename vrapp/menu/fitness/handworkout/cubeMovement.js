8e62c5d3de8d3357cea0da12551c73149a30f931
Aditya Gadepalli <adityag@zenoti.com>
2023-08-26 03:17:44 +0530

    reverted the handwork out game

--- vrapp/menu/fitness/handworkout/cubeMovement.js
+++ vrapp/menu/fitness/handworkout/cubeMovement.js
@@ -1,12 +1,10 @@
+
 AFRAME.registerComponent('cubemovement', {
     init: function() {
       this.position = this.el.getAttribute('position');
-      this.rotation = this.el.getAttribute('rotation');
     },
     
     tick: function (time, timeDelta) {
-      this.position.z += 0.02 * timeDelta;
-      this.rotation.y += 1;
-      this.rotation.y = (this.rotation.y % 90);
+      this.position.z += 0.01 * timeDelta;
     }
   });
--- vrapp/menu/fitness/handworkout/cubeSpawner.js
+++ vrapp/menu/fitness/handworkout/cubeSpawner.js
@@ -1,73 +1,39 @@
 // random num generator
 function getRandomNumber(value, offset) {
-  return Math.floor(Math.random() * value + offset);
-}
-
-// get either red or blue
-function getRedOrBlue() {
-  return getRandomNumber(10, 0) > 5 ? "blue" : "red";
-}
-
-AFRAME.registerComponent("cubespawner", {
-  init: function () {
-    this.timeStep = 0;
-    this.isLeft = true;
-    this.isTop = true;
-  },
-
-  tick: function (time, timeDelta) {
-    if (this.timeStep < 1000) {
-      this.timeStep += timeDelta;
-      return;
-    }
-    this.timeStep = 0;
-
-    var cubeEl = this.el.components.pool__cubes.requestEntity();
-    if (!cubeEl) { 
-      return;
-    }
-
-    // var positionX = getRandomNumber(-3, 3);
-    let positionX = 2;
-    var positionY = 2;
-    var positionZ = -25;
-
-    let testPositionY = 0.1;
-    let testPositionX = 0.2;
-
-    if(this.isTop) {
-      positionY = testPositionY;
-      if(this.isLeft) {
-        positionX = testPositionX;
-        this.isLeft = false;
-      } else {
-        positionX = -testPositionX;
-        this.isLeft = true;
-        this.isTop = false;
-      }
-    } else {
-      positionY = testPositionY;
-      if(this.isLeft) {
-        positionX = testPositionX;
-        this.isLeft = false;
-      } else {
-        positionX = -testPositionX;
-        this.isLeft = true;
-        this.isTop = true;
-      }
-    }
+    return Math.floor(Math.random() * value + offset);
+  }
+  
+  // get either red or blue
+  function getRedOrBlue() {
+    return getRandomNumber(10, 0) > 5 ? "blue" : "red";
+  }
+  
+  AFRAME.registerComponent('cubespawner', {
+    init: function() {
+      this.timeStep = 0;
+    },
     
-
-    var cubeColor = getRedOrBlue();
-
-    cubeEl.setAttribute("position", {
-      x: positionX,
-      y: positionY,
-      z: positionZ,
-    });
-    cubeEl.setAttribute("rotation", { x: -45, y: 0, z: 0 });
-    cubeEl.setAttribute("material", `color: ${cubeColor}`);
-    cubeEl.setAttribute("class", "cubes");
-    cubeEl.play();
-  },
-});
+    tick: function (time, timeDelta) {
+      if (this.timeStep < 1000) {
+        this.timeStep += timeDelta;
+        return;
+      }
+      this.timeStep = 0;
+      
+      var cubeEl = this.el.components.pool__cubes.requestEntity();
+      if (!cubeEl) {
+        return;
+      }
+      
+      var positionX = getRandomNumber(3, -1);
+      var positionY = getRandomNumber(2, 1);
+      var positionZ = -25;
+      
+      var cubeColor = getRedOrBlue();
+      
+      cubeEl.setAttribute('position', {x: positionX, y: positionY, z: positionZ});
+      cubeEl.setAttribute('material', `color: ${cubeColor}`);
+      cubeEl.setAttribute('class', 'cubes');
+      cubeEl.play();
+    }
+  });
