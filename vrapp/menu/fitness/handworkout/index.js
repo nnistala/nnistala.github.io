95c01b4a946322264fbf212af23ca6e212fa78ea
Aditya Gadepalli <adityag@zenoti.com>
2023-08-27 15:59:26 +0530

    added a start button in handwork out

--- vrapp/menu/fitness/handworkout/index.js
+++ vrapp/menu/fitness/handworkout/index.js
@@ -11,3 +11,11 @@ function getUserPosition() {
     var worldPosition = new THREE.Vector3();
     return userEntity.object3D.getWorldPosition(worldPosition);
 }
+
+function startPlay() {
+  var startBtn = document.querySelector('#startPlay');
+  startBtn.remove();
+
+  var scene = document.querySelector('a-scene');
+  scene.setAttribute('cubeSpawner');
+}
